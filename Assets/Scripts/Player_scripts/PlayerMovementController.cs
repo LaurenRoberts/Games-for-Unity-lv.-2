3be24e23ddc6695234c60e0efc17b40121194496
Lauren <Suzywang12@yahoo.com>
2018-06-09 12:43:46 -0500

    the player has a visual representation and can move

---
+++ Assets/Scripts/Player_scripts/PlayerMovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update () {
+       
+        if (Input.GetButton("MoveRight") == true)
+        {
+            Debug.Log("move right");
+            //Moves (the player) right
+            transform.position += new Vector3(.5f,0f,0f);
+        }
+        if (Input.GetButton("MoveLeft") == true)
+        {
+            Debug.Log("move left");
+            //Moves (the player) right
+            transform.position += new Vector3(-.5f, 0f, 0f);
+        }
+    }
+}
