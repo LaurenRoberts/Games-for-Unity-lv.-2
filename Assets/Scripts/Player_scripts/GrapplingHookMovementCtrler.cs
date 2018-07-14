792e14f797c3e720c98830d58a00da5b98f3e597
Lauren <Suzywang12@yahoo.com>
2018-07-14 12:29:10 -0500

    worked on grapple hook more. moves now

--- Assets/Scripts/Player_scripts/GrapplingHookMovementCtrler.cs
+++ Assets/Scripts/Player_scripts/GrapplingHookMovementCtrler.cs
@@ -3,28 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class GrapplingHookMovementCtrler : MonoBehaviour {
 
     public Rigidbody2D GrappleBody;
-    public float GrappleForceX = 8f;
+    public GameObject Grapplehook;
+    public GameObject Player;
     private Vector3 Origin;
     private float DeltaMove;
-    object Grapplehook = GameObject.Find("Grapplehook");
- 
+    private float PositionDifference;
+    public float GrappleForceX = 10f;
+    public float GrappleForceY = 9f;
+    public float GHslow = 5f;
+    public float RopeDistance = 5f;
+
+
+
     private void Start()
     {
-        
+        Grapplehook = GameObject.Find("Grapplehook");
+        GrappleBody = GetComponent<Rigidbody2D>();
+        Player = GameObject.Find("Player");
+
     }
 
+    private void FindPosition() {
+        Vector3 Grap = Grapplehook.transform.position;
+        Vector3 plyr = Player.transform.position;
+        PositionDifference = Vector3.Distance(Grap, plyr);
+    }
 
+    private void FixedUpdate()
+    {
+        FindPosition();
+        if (Input.GetButtonDown("Grapple") && PositionDifference < RopeDistance)
+        {
+            Origin = Grapplehook.transform.position;
+            GrappleBody.AddForce(new Vector3(GrappleForceX, GrappleForceY, 0f));
+            GrappleBody.drag = GHslow;
+        }
+        if (PositionDifference >= RopeDistance)
+        {
+            GrappleBody.AddForce(new Vector3(-GrappleForceX, -GrappleForceY, 0f));
+            //(code)find fastest course to player and add force in that direction
+            //  GrappleBody.MovePosition(Player.transform.position);
+            //Move Grapplehook to Player X & Y
+        }
+
+    } 
     // Update is called once per frame
     void Update () {
 
-        if (Input.GetButtonDown("Grapple"))
-        {
-          //  Origin = Grapplehook.transform.position;
-            GrappleBody.AddForce(new Vector3(GrappleForceX, 0f, 0f));
-        }
+        
 
         /*what I need to do, in psudo-code:
          when(buttonpressed) {
@@ -32,6 +62,6 @@ public class GrapplingHookMovementCtrler : MonoBehaviour {
         }
         if(runs into interactable thing){
        Stop + bring [player] to collision area 
-        }*/
+        }else{go back to player}*/
     }
 }
